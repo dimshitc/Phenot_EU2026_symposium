b99cf468b562f1ed5304b01e9a449f4da9a5c267
Dmytro Dymshyts <ddymshyt@its.jnj.com>
2026-04-14 07:55:27 -0400

    Add pivot examples for PhenotypeLog, CohortDefinitionSet, and ConceptDefinitionSet

--- OHDSI_PL_usage_example.R
+++ OHDSI_PL_usage_example.R
@@ -4,6 +4,24 @@ library(PhenotypeLibrary)
 
 PhenotypeLog<-getPhenotypeLog()
 
+#see all cohort metadata
+cohortMetadata <-
+PhenotypeLog %>%
+  filter(cohortId == 123) %>%
+  mutate(across(everything(), as.character)) %>%
+  tidyr::pivot_longer(cols = everything(), names_to = "columnName", values_to = "value") %>%
+  as.data.frame()
+
 CohortDefinitionSet<-getPlCohortDefinitionSet(123)
 
+CohortDefinitionPivot <- CohortDefinitionSet %>%
+  mutate(across(everything(), as.character)) %>%
+  tidyr::pivot_longer(cols = everything(), names_to = "columnName", values_to = "value") %>%
+  as.data.frame()
+
 ConceptDefinitionSet<-getPlConceptDefinitionSet(123)
+
+ConceptDefinitionPivot <- ConceptDefinitionSet %>%
+  mutate(across(everything(), as.character)) %>%
+  tidyr::pivot_longer(cols = everything(), names_to = "columnName", values_to = "value") %>%
+  as.data.frame()
